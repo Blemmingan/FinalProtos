5ebd7f607a40c5178364a18d62fa5ee8e38650ed
rdintino <rdintino@itba.edu.ar>
2024-11-16 18:44:24 -0300

    Adding POP3 parser definition

--- src/parser/pop3_parser.h
+++ src/parser/pop3_parser.h
@@ -17,6 +17,11 @@ enum pop3_states{
    ERROR_STATE
 };
 
+typedef struct {
+    const struct parser_state_transition *transitions;  // Transiciones de este estado
+    size_t transition_count;                            // Número de transiciones para este estado
+} state_definition;
+
 static void process_command(parser_event *event, const uint8_t c);
 static void process_argument(parser_event *event, const uint8_t c);
 static void finalize(parser_event *event, const uint8_t c);
@@ -65,4 +70,21 @@ static const struct parser_state_transition TERMINATION_TRANSITION[] = {
 static const struct parser_state_transition ERROR_TRANSITION[] = {
     { .when = ANY, .dest = ERROR_STATE, .act1 = NULL},
     { .when = '\r', .dest = TERMINATION_STATE, .act1 = NULL}
+};
+
+static const state_definition state_definitions[MAX_STATES] = {
+    [INTITAL_STATE] = { INIT_TRANSITION, N(INIT_TRANSITION) },
+    [FIRST_CHAR_STATE] = { FIRST_CHAR_TRANSITION, N(FIRST_CHAR_TRANSITION) },
+    [SECOND_CHAR_STATE] = { SECOND_CHAR_TRANSITION, N(SECOND_CHAR_TRANSITION) },
+    [THIRD_CHAR_STATE] = { THIRD_CHAR_TRANSITION, N(THIRD_CHAR_TRANSITION) },
+    [FOURTH_CHAR_STATE] = { FOURTH_CHAR_TRANSITION, N(FOURTH_CHAR_TRANSITION) },
+    [ARGS_STATE] = { ARGS_TRANSITION, N(ARGS_TRANSITION) },
+    [TERMINATION_STATE] = { TERMINATION_TRANSITION, N(TERMINATION_TRANSITION) },
+    [ERROR_STATE] = { ERROR_TRANSITION, N(ERROR_TRANSITION) }
+};
+
+const struct parser_definition pop3_parser = {
+    .start_state = INTITAL_STATE,
+    .states = state_definitions,
+    .states_count = MAX_STATES,
 };
